408f7937a92e061bb05458577b283de1b52ec580
NF RIDOY <nfridoy@gmail.com>
2023-11-01 03:29:04 +0600

    .env DB set

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,10 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +29,8 @@ async function run() {
 
     // const productCollection = client.db('emaJohnDB').collection('products');
     await client.connect(); // Use for dev Not for Deploy
+
+    
 
     const productCollection = client.db('emaJohnDB').collection('products');
 
